165b2744eca5c71f36a09a7b8e7062a3e837e7eb
Viery Nugroho <viery15102002@gmail.com>
2024-05-13 21:41:48 +0700

    update: skeleton

--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Skeleton } from '@/components/ui/skeleton';
 import AddProduct from './AddProduct';
@@ -6,7 +6,7 @@ import EditProduct from './EditProduct';
 import DeleteProduct from './DeleteProduct';
 import Profile from './Profile';
 
-import useSWR, { useSWRConfig } from 'swr';
+// import useSWR, { useSWRConfig } from 'swr';
 
 const ProductList = () => {
 	//! old
@@ -40,41 +40,43 @@ const ProductList = () => {
 	// const { data, error, isLoading } = useSWR('products', fetchProduct);
 
 	return (
-		<div className='flex flex-col mt-5 container'>
+		<div className='container flex flex-col mt-5'>
 			<div className='w-full'>
 				<div className='flex'>
 					<AddProduct />
 					<Profile />
 				</div>
-				<div className='relative shadow rounded-lg mt-5'>
+				<div className='relative mt-5 rounded-lg shadow'>
 					<table className='w-full text-sm text-left text-white'>
 						<thead className='text-xs text-white uppercase bg-black'>
 							<tr>
-								<th className='py-3 px-1 text-center'>No</th>
-								<th className='py-3 px-6 text-center'>Name</th>
-								<th className='py-3 px-6 text-center'>Price</th>
-								<th className='py-3 px-1 text-center'>Action</th>
+								<th className='px-1 py-3 text-center'>No</th>
+								<th className='px-6 py-3 text-center'>Name</th>
+								<th className='px-6 py-3 text-center'>Price</th>
+								<th className='px-1 py-3 text-center'>Action</th>
 							</tr>
 						</thead>
 						<tbody>
 							{loading ? (
 								<tr className='border-b'>
-									<td className='py-3 px-1 text-black text-center'>
+									<td className='px-1 py-3 text-center text-black'>
 										<Skeleton className='h-6 bg-slate-300' />
 									</td>
-									<td className='py-3 px-1 text-black text-center'>
+									<td className='px-1 py-3 text-center text-black'>
 										<Skeleton className='h-6 bg-slate-300' />
 									</td>
-									<td className='py-3 px-1 text-black text-center'>
+									<td className='px-1 py-3 text-center text-black'>
+										<Skeleton className='h-6 bg-slate-300' />
+									</td>
+									<td className='px-1 py-3 text-center text-black'>
 										<Skeleton className='h-6 bg-slate-300' />
 									</td>
 								</tr>
 							) : data.length <= 0 ? (
 								<tr>
 									<td
-										className='py-3 px-6 text-center bg-red-700 text-white font-bold'
-										colSpan='4'
-									>
+										className='px-6 py-3 font-bold text-center text-white bg-red-700'
+										colSpan='4'>
 										Empty Product
 									</td>
 								</tr>
@@ -82,12 +84,11 @@ const ProductList = () => {
 								data.map((product, index) => (
 									<tr
 										className='border-b'
-										key={product.id}
-									>
-										<td className='py-3 px-1 text-black text-center'>{index + 1}</td>
-										<td className='py-3 px-6 text-black font-bold'>{product.name}</td>
-										<td className='py-3 px-6 text-black'>{product.price}</td>
-										<td className='py-3 px-1 text-center'>
+										key={product.id}>
+										<td className='px-1 py-3 text-center text-black'>{index + 1}</td>
+										<td className='px-6 py-3 font-bold text-black'>{product.name}</td>
+										<td className='px-6 py-3 text-black'>{product.price}</td>
+										<td className='px-1 py-3 text-center'>
 											<EditProduct
 												productId={product.id}
 												productName={product.name}
